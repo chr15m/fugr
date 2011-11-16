2801eb16fea992d75a5c09f72146eb4f206daabd
Chris McCormick <chris@mccormick.cx>
2011-11-16 17:07:48 +0800

    Feeds are now imported into Django models associated with the user (UserFeed, FeedTag, Feed).

--- django_fugr/models.py
+++ django_fugr/models.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class FeedTags(models.Model):
+class FeedTag(models.Model):
 	tag = models.CharField(max_length=256)
+	
+	def __unicode__(self):
+		return unicode(self.tag)
 
 class Feed(models.Model):
 	title = models.CharField(max_length=1024)
@@ -10,9 +13,15 @@ class Feed(models.Model):
 	blog_url = models.CharField(max_length=1024)
 	# text = 
 	# flavour = 
+	
+	def __unicode__(self):
+		return unicode(self.title)
 
-class UserFeeds(models.Model):
+class UserFeed(models.Model):
 	user = models.ForeignKey(User)
 	feed = models.ForeignKey(Feed)
-	tags = models.ManyToManyField(FeedTags)
+	tags = models.ManyToManyField(FeedTag)
+	
+	def __unicode__(self):
+		return unicode(self.user) + " - " + unicode(self.feed) + " " + str(self.tags.all())
 
--- django_fugr/views.py
+++ django_fugr/views.py
@@ -5,6 +5,8 @@ from django.core.urlresolvers import reverse
 
 from parse_opml import parse_opml
 
+from models import Feed, FeedTag, UserFeed
+
 @login_required
 def index(request):
 	return direct_to_template(request, "index.html", {})
@@ -16,7 +18,30 @@ def opml_upload(request):
 		opmldata = parse_opml(uploaded.read())
 		# create or modify feed objects for this opml data
 		uploaded.close()
-	from pprint import pformat
-	return HttpResponse(pformat(opmldata), mimetype="text/plain")
-	#return HttpResponseRedirect(reverse("index"))
+		# what the inside of each tuple looks like
+		# u'http://bitcoinmorpheus.tumblr.com/rss': {'blog_url': u'http://bitcoinmorpheus.tumblr.com/',
+                #                            'labels': set([u'bitcoin',
+                #                                           u'economics']),
+                #                            'title': u'Bitcoin Morpheus'},
+		# go through every feed creating the stuff
+		for feed_url in opmldata:
+			# get or create the tags listed against this feed for this user
+			tags = []
+			for tag in opmldata[feed_url]["labels"]:
+				tagobject, created = FeedTag.objects.get_or_create(tag=tag)
+				tags.append(tagobject)
+				tagobject.save()
+				print 'FeedTag:', tagobject
+			# get or create the feed
+			feed, created = Feed.objects.get_or_create(feed_url=feed_url, title=opmldata[feed_url]["title"], blog_url=opmldata[feed_url]["blog_url"])
+			feed.save()
+			print 'Feed:', feed
+			# now assign this feed with these tags to the current user
+			uf, created = UserFeed.objects.get_or_create(user=request.user, feed=feed)
+			uf.tags = tags
+			uf.save()
+			print 'UserFeed:', feed
+	#from pprint import pformat
+	#return HttpResponse(pformat(opmldata), mimetype="text/plain")
+	return HttpResponseRedirect(reverse("index"))
 

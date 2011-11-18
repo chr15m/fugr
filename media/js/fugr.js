ea8f8718e0c5d1b70269d3b84f55968d08345ae7
Chris McCormick <chris@mccormick.cx>
2011-11-18 16:08:06 +0800

    Got linked tags with icons and feed list with icons, plus bad-looking header bar with back and refresh buttons.

--- media/js/fugr.js
+++ media/js/fugr.js
@@ -5,6 +5,9 @@ $(function(){
 		"feeds": {}
 	};
 	
+	// this header will hang out at the top of the read area
+	var headerhtml = $("<div class='read-header'><button id='read-back' class='ui-widget ui-button'><span class='ui-icon ui-icon-circle-arrow-w'></span></button><button id='read-refresh' class='ui-widget ui-button'><span class='ui-icon ui-icon-arrowrefresh-1-e'></span></button></div>");
+	
 	// jquery-ui styles
 	$('.tabs').tabs();
 	$('button,input[type=submit]').button();
@@ -21,21 +24,48 @@ $(function(){
 		$('form#opml-upload').submit();
 	});
 	
+	// populates the read tab with this tag's feeds
+	function populate_read_tab_with_feeds(tagname) {
+		// empty the area
+		$("div#tab-read").html(headerhtml);
+		// add the link to summary feeds
+		$("div#tab-read").append("<div class='feed-link'>All " + tagname + "</div>");
+		var tagfeeds = session.tags[tagname];
+		// now put the feeds in there
+		for (var f=0; f<tagfeeds.length; f++) {
+			$("div#tab-read").append("<div class='feed-link'>" + tagfeeds[f].title + "</div>");
+		}
+		// add the folder icons
+		$("div.feed-link").addClass("ui-state-default");
+		$("div.feed-link").prepend("<span class='ui-icon ui-icon-signal-diag'></span>");
+		// make them clickable
+		$("div.feed-link").click(function(e) {
+			
+		});
+	}
+	
 	// populates the read tab with this user's tags
 	function populate_read_tab_with_tags(tags) {
 		// empty the area
-		$("div#tab-read").html("");
+		$("div#tab-read").html(headerhtml);
 		// default 'tag' links
-		$("div#tab-read").append($("<div class='feedtag'>All Items</div>"));
-		$("div#tab-read").append($("<div class='feedtag'>Read Items</div>"));
-		$("div#tab-read").append($("<div class='feedtag'>Starred Items</div>"));
-		$("div#tab-read").append($("<div class='feedtag'>People You Follow</div>"));
+		$("div#tab-read").append($("<div class='feedtag-link'>All Items</div>"));
+		$("div#tab-read").append($("<div class='feedtag-link'>Read Items</div>"));
+		$("div#tab-read").append($("<div class='feedtag-link'>Starred Items</div>"));
+		//$("div#tab-read").append($("<div class='feedtag-link'>People You Follow</div>"));
 		//$("div#tab-read").append($("<div class='feedtag'>Recommended Items</div>"));
 		// now put the tags in there
 		for (var tag in tags) {
-			var tagdiv = $("<div class='feedtag'>" + tag + "</div>");
-			$("div#tab-read").append(tagdiv);
+			var tagdiv = $();
+			$("div#tab-read").append("<div class='feedtag-link'>" + tag + "</div>");
 		}
+		// add the folder icons
+		$("div.feedtag-link").addClass("ui-state-default");
+		$("div.feedtag-link").prepend("<span class='ui-icon ui-icon-folder-open'></span>");
+		// make them clickable
+		$("div.feedtag-link").click(function(e) {
+			populate_read_tab_with_feeds($(this).text());
+		});
 	}
 	
 	// load up our list of tags into the main pane

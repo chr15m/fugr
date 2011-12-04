8487622b9d34b73f448df165a5000e42018ada82
Chris McCormick <chris@mccormick.cx>
2011-12-04 21:01:51 +0800

    Script to flush latest cached data from all feeds.

---
+++ django_fugr/management/commands/fugrflushfeeds.py
@@ -0,0 +1,19 @@
+from sys import exit
+from multiprocessing import Pool
+
+from django.core.management.base import NoArgsCommand, CommandError
+
+from django_fugr.models import FeedData
+
+import settings
+
+class Command(NoArgsCommand):
+	help = 'Flush all cached data from fugr feeds (does not delete old entries).'
+	def handle_noargs(self, *args, **kwargs):
+		for f in FeedData.objects.all():
+			print 'Resetting "%s" <%s>' % (f.feed.title, f.feed.blog_url)
+			f.parsed = None
+			f.etag = None
+			f.last_modified = None
+			f.save()
+
